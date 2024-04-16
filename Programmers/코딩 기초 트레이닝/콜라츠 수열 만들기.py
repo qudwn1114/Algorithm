2d873fb3159c6ddc051d37fbdd207df0db9cfe24
qudwn1114 <qudwn1114@gmail.com>
2024-04-16 13:07:28 +0900

    Add 콜라츠 수열 만들기

---
+++ Programmers/코딩 기초 트레이닝/콜라츠 수열 만들기.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer = [n]
+    while(True):
+        if n == 1:
+            break
+            
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1            
+        answer.append(n)
+            
+    return answer
