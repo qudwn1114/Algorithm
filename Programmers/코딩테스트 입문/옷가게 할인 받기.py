fd6ee1737d0de380f2e5d2bdaeabb71c74a34e3a
qudwn1114 <qudwn1114@gmail.com>
2024-04-17 21:40:24 +0900

    Add 옷가게 할인 받기

---
+++ Programmers/코딩테스트 입문/옷가게 할인 받기.py
@@ -0,0 +1,10 @@
+def solution(price):
+    answer = price
+    if price >= 100000 and price < 300000:
+        answer = price * 0.95
+    elif price >= 300000 and price < 500000:
+        answer = price * 0.9
+    elif price >= 500000:
+        answer = price * 0.8
+        
+    return int(answer)
